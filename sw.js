c1bd76d1e91ae995d79fad0d10f283f6bf96f80e
Victor Carvajal <ecos.moda.oficial@gmail.com>
2026-04-03 14:01:24 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'ecos-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
